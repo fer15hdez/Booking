2d1e1204440c2f189d811416ba39295aff5ecd5a
Fernando <fernan.hdez2015@gmail.com>
2025-05-05 23:38:44 -0300

    Refactor: more

--- booking/src/main/java/com/booking/service/RoomTypeService.java
+++ booking/src/main/java/com/booking/service/RoomTypeService.java
@@ -3,13 +3,17 @@ package com.booking.service;
 import com.booking.domain.RoomType;
 import com.booking.domain.RoomTypeDTO;
 import com.booking.domain.RoomTypeRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
 
 @Service
 public class RoomTypeService {
 
     private final RoomTypeRepository repository;
     private final RoomTypeMapper mapper;
+    Logger logger =  Logger.getLogger(getClass().getName());
 
     public RoomTypeService(RoomTypeRepository repository, RoomTypeMapper mapper) {
         this.repository = repository;
@@ -18,6 +22,13 @@ public class RoomTypeService {
 
     public RoomType createRoomType(RoomTypeDTO roomTypeDTO){
         RoomType roomType = this.mapper.toRoomType(roomTypeDTO);
+
+        this.repository.findById(roomType.getId()).orElseThrow(
+                () -> {
+                    logger.info("No found RoomType entity with id: " + roomType.getId());
+                    return new EntityNotFoundException("No found Room Type entity with given id");
+                }
+        );
         return this.repository.save(roomType);
     }
 }
